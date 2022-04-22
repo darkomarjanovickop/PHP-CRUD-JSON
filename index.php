dd2ba337c60d6592e6460279bbc73b8403c0c8ff
Darko <darkomarjanovickop@gmail.com>
2022-04-22 19:59:34 +0200

    JSON CRUD operations

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+require "./users/users.php";
+$users = getUsers();
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Simple PHP CRUD</title>
+</head>
+
+<body>
+
+    <table class="table">
+
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Website</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($users as $user):
+            ?>
+            <tr>
+                <td><?php echo $user['name']?></td>
+                <td><?php echo $user['username']?></td>
+                <td><?php echo $user['email']?></td>
+                <td><?php echo $user['phone']?></td>
+                <td><?php echo $user['website']?></td>
+                <td>
+                    <a href="view.php" class="btn btn-sm btn-outline-info">View</a>
+                    <a href="update.php" class="btn btn-sm btn-outline-secondary">Update</a>
+                    <a href="delete.php" class="btn btn-sm btn-outline-danger">Delete</a>
+
+                </td>
+            </tr>
+            <?php
+            endforeach;
+            ?>
+        </tbody>
+
+    </table>
+
+</body>
+
+</html>
